ed1eb92bccd3c158e267a26b86af3e17de7bcc81
Dane Thurber <dane.thurber@gmail.com>
2021-01-19 13:20:46 -0700

    feat(major-illusion): show/hide player panel

--- modules/major-illusion/src/customizer.ts
+++ modules/major-illusion/src/customizer.ts
@@ -5,17 +5,31 @@ import { MODULE_NAME, STYLESHEET_NAME, THEME_NAMES, VERSION } from './constants'
 
 import themes from './themes'
 
+interface UIPlayers {
+  close: () => void
+  render: (next: boolean) => void
+  rendered: boolean
+}
+
 export default class Customizer {
   public VERSION = VERSION
 
+  private root: HTMLElement
   private logger: NotificationsLogger
   private themeName: keyof typeof THEME_NAMES
 
   constructor(logger: NotificationsLogger) {
+    this.root = document.documentElement
+
     this.logger = logger
     this.logger.info('init')
 
     this.init()
+  }
+
+  public setPlayerPanelState(show = false): void {
+    if (show) this.showPlayerPanel()
+    else this.hidePlayerPanel()
   }
 
   private init() {
@@ -29,5 +43,21 @@ export default class Customizer {
 
     this.logger.info('applying theme', this.themeName)
     injectStylesheet(STYLESHEET_NAME, theme)
+  }
+
+  private hidePlayerPanel() {
+    const players: UIPlayers = (ui as any).players
+    this.logger.info('hiding players panel')
+    players.close()
+
+    this.root.setAttribute('data-mi-players-hide', 'true')
+  }
+
+  private showPlayerPanel() {
+    const players: UIPlayers = (ui as any).players
+    this.logger.info('showing players panel')
+    players.render(true)
+
+    this.root.removeAttribute('data-mi-players-hide')
   }
 }
--- modules/major-illusion/src/main.ts
+++ modules/major-illusion/src/main.ts
@@ -1,16 +1,29 @@
 import { NotificationsLogger } from 'foundryvtt-logger'
 
 import { MODULE_NAME } from './constants'
-import Customizer from './customizer'
 import { init as initSettings } from './settings'
 
-Hooks.on('init', function () {
+// TODO: change name to MajorIllusion
+import Customizer from './customizer'
+
+declare global {
+  interface Window {
+    majorIllusion: Customizer
+  }
+}
+
+function getInstance() {
+  return window.majorIllusion
+}
+
+Hooks.once('init', function () {
   const logger = new NotificationsLogger(MODULE_NAME)
   initSettings()
 
-  window['customizer'] = new Customizer(logger)
+  window.majorIllusion = new Customizer(logger)
 })
 
-Hooks.on('ready', function () {
-  //
+Hooks.once('ready', function () {
+  const majorIllusion = getInstance()
+  majorIllusion.setPlayerPanelState()
 })
